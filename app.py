34a500b8891096f2e7a712f51f22f34eebed3d5e
Amrish Sharma <amkusharma@axway.com>
2022-05-02 09:19:09 +0530

    app turned to flaskapp

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-import requests,json,random
+import requests
+import json
+import random
+from flask import Flask
 
-quote_url='https://raw.githubusercontent.com/Amrish-Sharma/FlipQuotes/83e1820ff6c33f17a7f9312389a45ff04e0917d1/app/src/main/assets/quotes.json'
+# variables
+quote_url = 'https://raw.githubusercontent.com/Amrish-Sharma/FlipQuotes/83e1820ff6c33f17a7f9312389a45ff04e0917d1/app/src/main/assets/quotes.json'
 
+# Flask app initialisation
+app = Flask(__name__)
+
+# randome quote page
+
+
+@app.route("/rq")
 def rqg():
-    response=requests.get(quote_url).json()
-    response_json=response['quotes']
-    random_quote=random.choice(response_json)
-    print(random_quote)
+    response = requests.get(quote_url).json()
+    response_json = response['quotes']
+    random_quote = random.choice(response_json)
+    return random_quote
+
+# index page
 
 
-def main():
-    rqg()
+@app.route("/")
+def index():
+    return "<h1>Welcome to RandomQuoteGenerator<h2>"
+
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
